54a44fe08e3124dde50d507d6e2c86a73efe94c3
Alex Scheller <alexgesc@gmail.com>
2019-02-23 20:32:34 -0800

    feature/bugfix: requirements file, fixed sed command in freeze

--- backend/dark_chess_api/cli.py
+++ backend/dark_chess_api/cli.py
@@ -7,4 +7,9 @@ def init(app):
 	@app.cli.command()
 	def freeze():
 		os.system('pip freeze > requirements.txt')
-		os.system('sed "/pkg-resources==0.0.0" requirements.txt')
+		os.system('sed -i "/pkg-resources==0.0.0/d" requirements.txt')
+
+	# installs from requirements file
+	@app.cli.command()
+	def requirements():
+		os.system('pip install -r requirements.txt')
