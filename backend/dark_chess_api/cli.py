ae9faae8e892eb373adfa44d187a94d270519dd2
Alex Scheller <alexgesc@gmail.com>
2019-03-09 21:02:13 -0800

    feature: Command to rebuild the database.

--- backend/dark_chess_api/cli.py
+++ backend/dark_chess_api/cli.py
@@ -1,7 +1,20 @@
+# This file contains a helpful set of cli commands. Generally speaking
+# they simply wrap various shell utilities.
+
 import os
 import click
 
 def init(app):
+
+	# deletes and rebuild the database from scratch
+	@app.cli.command()
+	def rebuilddb():
+		if app.config['CHOSEN_DATABASE'] == 'SQLITE':
+			os.system('rm app.db')
+			os.system('rm -rf migrations/')
+			os.system('flask db init')
+			os.system('flask db migrate')
+			os.system('flask db upgrade')
 
 	# removes the 'pkg-resources=0.0.0' bug with ubuntu
 	@app.cli.command()
@@ -12,4 +25,4 @@ def init(app):
 	# installs from requirements file
 	@app.cli.command()
 	def requirements():
-		os.system('pip install -r requirements.txt')
+		os.system('pip install -r requirements.txt --upgrade')
