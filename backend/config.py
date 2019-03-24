fbfd864b0f19bdd2f87b29ac6a03962211a68178
Alex Scheller <alexgesc@gmail.com>
2019-03-23 18:26:13 -0700

    feature: Added mysql db uri.

--- backend/config.py
+++ backend/config.py
@@ -8,8 +8,14 @@ class Config:
 
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'f82hy8bqk4qbl0g43gb9uq4fqvdjk904j3'
 
+	DB_USERNAME = os.environ.get('DB_USERNAME') or 'darkchess'
+	DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'darkchess'
+	DB_HOST = os.environ.get('DB_HOST') or 'localhost'
+	DB_NAME = os.environ.get('DB_NAME') or 'darkchess'
+
 	### database ###
 	DATABASE_URIS = {
+		'MYSQL' : f'mysql+mysqldb://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}',
 		'SQLITE' : 'sqlite:///' + os.path.join(basedir, 'app.db')
 	}
 	CHOSEN_DATABASE = os.environ.get('CHOSEN_DATABASE') or 'SQLITE'
