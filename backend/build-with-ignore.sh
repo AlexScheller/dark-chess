d49de31f2ae9571dd8cea2a3ab6cd5ff7d983d80
Alex Scheller <alex.scheller.mail@gmail.com>
2020-09-27 17:39:36 -0700

    getting backend ready for letsencrypt.

--- backend/build-with-ignore.sh
+++ backend/build-with-ignore.sh
@@ -1,8 +1,9 @@
-# This script exists because of my ignorance of docker/docker-compose
-# Attempting to build the darkchess-api image fails because it
-# doesn't have certain file permissions in the 'data/' folder,
-# however that folder can't be ignored by docker-compose so the
-# following .dockerignore can't stick around.
+# This script exists because docker doesn't have a nice way yet of seperate
+# `dockerignore`s for seperate `Dockerfile`s without turning on a specific
+# build flag and sub-naming your Dockerfile. Attempting to build the
+# darkchess-client image fails because it doesn't have certain file permissions
+# in the 'data/' folder, however that folder can't be ignored by docker-compose
+# so the following .dockerignore can't stick around.
 echo "data/" > .dockerignore
 docker build -t darkchess-api:latest .
 rm .dockerignore
