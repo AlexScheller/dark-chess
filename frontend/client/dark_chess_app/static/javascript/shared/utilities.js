4661675aa77ebbf3484053df1922ac99e7fd3657
Alex Scheller <alex.scheller.mail@gmail.com>
2020-10-15 17:31:48 -0700

    some preliminary work on changing favicons.

--- frontend/client/dark_chess_app/static/javascript/shared/utilities.js
+++ frontend/client/dark_chess_app/static/javascript/shared/utilities.js
@@ -5,6 +5,16 @@ class Utilities {
 		window.location = '/auth/logout';
 	}
 
+	changeFavicon(iconSrc) {
+		let ico = document.createElement('link');
+		let old = document.getElementById('dynamic-favicon');
+		ico.id = 'dynamic-favicon';
+		ico.rel = 'shortcut icon';
+		ico.href = iconSrc;
+		if (old) old.remove();
+		document.head.appendChild(ico);
+	}
+
 }
 
 let utilities = new Utilities();
