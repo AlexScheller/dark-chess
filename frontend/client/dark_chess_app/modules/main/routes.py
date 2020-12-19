f4b155b532f7f95ab6db53e80ce98bfd5f35cfed
Alex Scheller <alex.scheller.mail@gmail.com>
2020-12-18 17:37:32 -0800

    feature: Version history.

--- frontend/client/dark_chess_app/modules/main/routes.py
+++ frontend/client/dark_chess_app/modules/main/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from flask_login import current_user
+from flask_login import current_user, login_required
 from dark_chess_app.modules.main import main
 from dark_chess_app.modules.auth.forms import LoginForm
 
@@ -10,3 +10,13 @@ def index():
 		form = LoginForm()
 		return render_template('main/index.html', form=form)
 	return render_template('main/index.html')
+
+@main.route('/about')
+@login_required
+def about():
+	return render_template('main/about.html')
+
+@main.route('/version-history')
+@login_required
+def version_history():
+	return render_template('main/version_history.html')
